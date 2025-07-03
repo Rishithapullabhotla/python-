f24143cea943b279812e763d46d6722dd9d135f7
Rishithapullabhotla <rishithapullabhotla1@gmail.com>
2025-07-03 11:05:01 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,45 @@
+name = input("Enter name:")
+age = int(input("Enter age:"))
+print("hello "+ name + "youare"+ str(age) + "years old" )
+
+# a= int(input("Enter num:"))
+# print(a)
+# print(str(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
