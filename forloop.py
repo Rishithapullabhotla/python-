1f00be7575a8855887bb9b446117eace95db2388
Rishithapullabhotla <rishithapullabhotla1@gmail.com>
2025-07-03 14:30:20 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,27 @@
+"""
+for loop is used to iterate a sequence of items
+syntax for i in sequence:
+i is a varibale
+in is a operator
+sequence is a condition
+            statements
+
+x = ["IT", "NONIT", "Bussiness"]
+for  i in x:
+    print(i)
+"""
+
+for k in range(15):
+    if k==7:
+        continue
+    print(k)
+
+for k in range(15):
+    if k==11:
+        break
+    print(k)
+
+
+
+
+
