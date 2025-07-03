cf27ac593d12d683702ba1cd7527c8153574fb5e
Rishithapullabhotla <rishithapullabhotla1@gmail.com>
2025-07-03 14:31:05 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,22 @@
+"""
+Tuples is a collection of items/values/elements
+they are enclosed within the parenthesis or open brackets()seperated by(,)
+As tuples are immutable it mean we cannot change
+so when the data is fixed we can go with tuples...
+"""
+# eg-1
+
+mytuple=(13,12,11)
+print(type(mytuple))#<class tuple>
+mytuple2=()#emptytupke
+print(type(mytuple2))#<class tuple>
+mytuple3=(10)
+print(type(mytuple3))#<class int>
+
+"""
+Note: when you wanna create a tuple with single value it should be separated by so that the
+compiler treats as tuple or else it just treats as integer  
+"""
+#creation of tuples 
+#syntax: tuplevariabl=(val1,val2,val3........,valn)
+mytuple5
